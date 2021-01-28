60c6a24a710e1f70813841ec717f99135c4a209d
Sangseok Doe <sangseoksoftware@outlook.com>
2021-01-28 17:05:03 -0500

    Finished local state part one, two

---
+++ src/components/Movie.js
@@ -0,0 +1,40 @@
+import { gql, useMutation } from "@apollo/client";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
+
+const Container = styled.div`
+  height: 380px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  //overflow: hidden;
+  border-radius: 7px;
+`;
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+`;
+
+export default ({ id, bg, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={isLiked ? null : likeMovie}>{isLiked ? "Unlike" : "Like"}</button>
+    </Container>
+  );
+};
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,94 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
-export default () => <h1>Detail</h1>;
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Loading = styled.div`
+  animation: blink 0.5s linear infinite;
+
+  @keyframes blink {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 0.5;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? <Loading>Loading...</Loading> : data.movie.title}</Title>
+        <Subtitle>
+          {data?.movie?.language} {data?.movie ? "•" : ""} {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
+    </Container>
+  );
+};
